d2fc824d0782f5287d8337dc77e965317d983abf
Anuj Kumar Kheria <Anuj.Kheria@insightdelivered.com>
2023-08-04 21:24:37 +0530

    reponsive

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -6,13 +6,13 @@ const Hero:React.FC = () => {
         <a className='col-span-1 justify-self-start'><Instagram sx={{width:50,height:50,fill:'#f5f5f5'}}/></a>
         <div className='col-span-2 justify-self-end'><Facebook sx={{ width:50,height:50,fill:'#f5f5f5'}}/></div>
 
-        <div className=' col-span-5 relative'>
-        <div className=' place-self-center flex justify-center items-center overflow-y-hidden p-10 border-4 border-white'>
+        <div className=' col-span-6 relative border-4 w-9/12 h-3/5 justify-self-center text-center flex  border-white justify-center'>
+        <div className=' place-self-center flex justify-center items-center overflow-y-hidden p-10 place-center'>
         <h1 className='text-white text-[3rem]  welcome'>
         Welcome
         </h1> 
         </div>
-        <div className=' text-white absolute bottom-6 right-2.5 bg-blue-600 '>I am <div className='inline-block h-6 pr-2 bg-white overflow-y-hidden pl-1'><ul className='dynamic bg-white text-blue-600 inline-block font-bold overflow-y-hidden leading-6 pl-1'>
+        <div className=' text-white absolute sm:-bottom-3 lg: sm:right-3  bg-blue-600 '>I am <div className='inline-block h-6 pr-2 bg-white overflow-y-hidden pl-1'><ul className='dynamic bg-white text-blue-600 inline-block font-bold overflow-y-hidden leading-6 pl-1'>
         <li>Anuj</li>
         <li>a Developer</li>
         <li>a Web Designer</li>
