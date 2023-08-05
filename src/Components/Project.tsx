e693eb297214204751f9055f734efab815635249
Anuj Kumar Kheria <Anuj.Kheria@insightdelivered.com>
2023-08-05 13:38:31 +0530

    overflow fix

--- src/Components/Project.tsx
+++ src/Components/Project.tsx
@@ -4,15 +4,15 @@ import software from '../assets/pexels-markus-spiske-965345.jpg'
 const Project:React.FC = () => {
   return (
     <>
-    <h1 className='text-white text-6xl text-center m-10'>Projects</h1>
+    <h1 className='text-white text-5xl text-center m-10'>Projects</h1>
     <div className="flex flex-wrap gap-2 justify-evenly">
 <Card sx={{width:'50%',textAlign:'center', m:4}}>
   <CardMedia component="img" image={software} sx={{borderRadius:1,p:1}}/>
- <h3 className='text-blue-600 font-bold text-3xl p-2'>Software</h3>
+ <h3 className='text-blue-600 font-bold text-2xl p-2'>Software</h3>
 </Card>
 <Card sx={{width:'50%',textAlign:'center',m:4  }}>
   <CardMedia component="img" image={software} sx={{borderRadius:1,p:1}}/>
- <h3 className='text-blue-600 font-bold text-3xl p-2'>Software</h3>
+ <h3 className='text-blue-600 font-bold text-2xl p-2'>Software</h3>
 </Card>
     </div>
     </>
